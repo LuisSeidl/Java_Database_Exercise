0d193ca2864e68d5d0a26f2fe7bac9922bd598d3
Luis <luisseidl4@gmail.com>
2025-06-14 11:33:17 +0200

    Get DB Connection and First Return Set

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,25 @@
 package org.example;
+import java.sql.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Connection String has format: jdbc:driver://hostname:port/nameOfDB you can add ?currentSchema=schema after the name of the DB to specify the Schema
+        try{
+            Connection con = DriverManager.getConnection("Won't tell you haha");
+
+            System.out.println("Connected to database");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT bestellnr, zeit FROM bestellung");
+
+            while(rs.next()){ //Sets Cursor to the next row, returns True if there is a next row
+                System.out.println("Bestellung: " + rs.getString("bestellnr") + "wurde aufgegeben um " + rs.getString("zeit"));
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
