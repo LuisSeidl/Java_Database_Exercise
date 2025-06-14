853005f1030a840c597dca03107cf81f7e289347
Luis <luisseidl4@gmail.com>
2025-06-14 21:04:52 +0200

    Finished the Exercise

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,28 @@ public class Main {
 
         //Connection String has format: jdbc:driver://hostname:port/nameOfDB you can add ?currentSchema=schema after the name of the DB to specify the Schema
         try{
-            Connection con = DriverManager.getConnection("Won't tell you haha");
+            Connection con = DriverManager.getConnection("EYYYYY");
+
+            con.setAutoCommit(true); //We have to commit/Rollback ourselves if we set it to false
 
             System.out.println("Connected to database");
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT bestellnr, zeit FROM bestellung");
+            ResultSet rs = stmt.executeQuery("SELECT DISTINCT 'Bundesland', 'Station' FROM bahnhoefe LIMIT 10");
 
             while(rs.next()){ //Sets Cursor to the next row, returns True if there is a next row
-                System.out.println("Bestellung: " + rs.getString("bestellnr") + "wurde aufgegeben um " + rs.getString("zeit"));
+                System.out.println("Station: " + rs.getString(2) + " ist in " + rs.getString(1));
             }
+
+            rs = stmt.executeQuery("SELECT COUNT(*) FROM bahnhoefe");
+            rs.next(); //We have to call rs.next to actually get to the result of the Query
+            System.out.println("Anzahl Bahnhoefe: " + rs.getInt(1)); //To get the Result of a Count query, we have to specify that we
+                                                                                // want to get the integer of the first row as the result
+                                                                                //(as there is only one row and column as result)
+
+            rs.close();
+            stmt.close();
+            con.close();
 
 
         } catch (SQLException e) {
